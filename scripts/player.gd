b15ada6ddb545452fbd949acfab974c54b5ec153
Nathan Harris <pixelfac.nerfbros@gmail.com>
2025-07-09 16:45:40 -0400

    add brackeys protocontroller

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,7 +2,6 @@ extends CharacterBody3D
 
 
 const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
 
 
 func _physics_process(delta: float) -> void:
@@ -10,18 +9,14 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
   
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir := Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
+	if direction: #direction is TRUE if nonzero
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
-	else:
+	else: #if no move input, lerp speed towards 0
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
