90a0590ffd33bf02b4d63af2700c17a2ecd2637b
Nathan Harris <pixelfac.nerfbros@gmail.com>
2025-07-09 17:22:43 -0400

    tweak player tank

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,35 +5,23 @@
 
 extends CharacterBody3D
 
-## Can we move around?
 @export var can_move : bool = true
-## Are we affected by gravity?
 @export var has_gravity : bool = true
-## Can we hold to run?
 @export var can_sprint : bool = false
 
 @export_group("Speeds")
-## Normal speed.
 @export var base_speed : float = 7.0
-## How fast do we run?
 @export var sprint_speed : float = 10.0
 
 @export_group("Input Actions")
-## Name of Input Action to move Left.
 @export var input_left : String = "move_left"
-## Name of Input Action to move Right.
 @export var input_right : String = "move_right"
-## Name of Input Action to move Forward.
 @export var input_forward : String = "move_up"
-## Name of Input Action to move Backward.
 @export var input_back : String = "move_down"
-## Name of Input Action to Sprint.
 @export var input_sprint : String = "sprint"
 
-var move_speed : float = 0.0
+var current_speed : float = 0.0
 
-## IMPORTANT REFERENCES
-@onready var collider: CollisionShape3D = $CollisionShape3D
 
 func _ready() -> void:
 	check_input_mappings()
@@ -46,20 +34,19 @@ func _physics_process(delta: float) -> void:
 
 	# Modify speed based on sprinting
 	if can_sprint and Input.is_action_pressed(input_sprint):
-			move_speed = sprint_speed
+		current_speed = sprint_speed
 	else:
-		move_speed = base_speed
+		current_speed = base_speed
+		
 
 	# Apply desired movement to velocity
 	if can_move:
 		var input_dir := Input.get_vector(input_left, input_right, input_forward, input_back)
 		var move_dir := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 		if move_dir:
-			velocity.x = move_dir.x * move_speed
-			velocity.z = move_dir.z * move_speed
+			velocity.x = current_speed
 		else:
-			velocity.x = move_toward(velocity.x, 0, move_speed)
-			velocity.z = move_toward(velocity.z, 0, move_speed)
+			velocity.x = move_toward(velocity.x, 0, 0.25)
 	else:
 		velocity.x = 0
 		velocity.y = 0
