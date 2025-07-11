365322244d1af38d81bc78c8cfcb1db905c102a1
Nathan Harris <pixelfac.nerfbros@gmail.com>
2025-07-11 14:33:34 -0400

    resize player model

--- scenes/turret.gd
+++ scenes/turret.gd
@@ -6,7 +6,6 @@ var camera : Camera3D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	camera = get_viewport().get_camera_3d()
-	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
