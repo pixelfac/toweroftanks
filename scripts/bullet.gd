ea32acbb0ab109719aed5318513806390563036e
Nathan Harris <pixelfac.nerfbros@gmail.com>
2025-07-11 18:45:38 -0400

    delete pickup on bullet collide

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -17,11 +17,24 @@ func _physics_process(delta: float) -> void:
 	var collision_data := move_and_collide(velocity * delta)
 	if collision_data:
 		print("collision")
-		if current_bounces > 0:
-			velocity = velocity.bounce(collision_data.get_normal())
-			rotation.y = Vector3.FORWARD.signed_angle_to(velocity.normalized(), Vector3.UP)
-			current_bounces -= 1
-		else:
-			queue_free()
-			print("bullet explode")
+		var collider : PhysicsBody3D = collision_data.get_collider()
+		if not is_instance_valid(collider):
+			return
+
+		match collider.collision_layer:
+			8: # (layer 4  - 1)^2 = 8
+				if current_bounces > 0:
+					velocity = velocity.bounce(collision_data.get_normal())
+					rotation.y = Vector3.FORWARD.signed_angle_to(velocity.normalized(), Vector3.UP)
+					current_bounces -= 1
+				else:
+					queue_free()
+					print("bullet explode")
+			16: # (layer 5  - 1)^2 = 15
+				print("direct hit!")
+				collider.queue_free()
+				queue_free()
+			_:
+				print("uncaught collision")
+				print("collision layer:", collider.collision_layer)
 	
--- scripts/pickup.gd
+++
@@ -1,5 +0,0 @@
-extends Area3D
-
-func _on_body_entered(_body: Node3D) -> void:
-	print("collision with pickup")
-	queue_free()
