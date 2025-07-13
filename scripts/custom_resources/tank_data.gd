f7e040c72c0be53bbdc1b9149e00a3ceda6b0bca
Nathan Harris <pixelfac.nerfbros@gmail.com>
2025-07-13 19:58:41 -0400

    add tank data class

---
+++ scripts/custom_resources/tank_data.gd
@@ -0,0 +1,10 @@
+extends Resource
+class_name TankData
+
+@export var base_speed : float
+@export var boost_speed : float
+@export var fire_rate : float
+@export var deceleration : float
+@export var turn_speed : float
+@export var bullet_speed : float
+@export var bullet_bounces : int
